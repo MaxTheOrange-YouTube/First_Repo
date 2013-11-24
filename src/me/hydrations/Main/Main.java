ec5adfdea608c5f09ea13bfa52fd10b5200afbca
MaxTheOrange-YouTube <maxholzmann@gmail.com>
2013-11-24 08:28:51 -0600

    Added Main class, first commit.

---
+++ src/me/hydrations/Main/Main.java
@@ -0,0 +1,17 @@
+package me.hydrations.Main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	public void onEnable() {
+		// This is a github for my youtube tutorial
+		// www.youtube.com/MaxTheOrange
+		// This is repo is not going to be used.
+	}
+	
+	public void onDisable() {
+		
+	}
+
+}
